650e5c1d87c69735151baf09ea2017db9c003017
WDsagor <sagarwpress@gmail.com>
2022-06-13 03:19:47 +0600

    add delivery api

--- index.js
+++ index.js
@@ -64,17 +64,36 @@ async function run() {
     app.put("/inventory/:id", async (req, res) => {
       const items = req.body;
       const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      // console.log(items);
+      const filter = { _id: ObjectId(id) };
       const updateItem ={
         $set: items
       }
-      const result = await itemCollection.updateOne(query, updateItem, options);
-      // console.log(result);
+      // console.log(updateItem);
+      const result = await itemCollection.updateOne(filter, updateItem);
+      console.log(result);
       res.send({
         success: true,
-        message: `Successfully Update ${updateItem.name}!`,
+        message: `Successfully Update ${items.name}!`,
+      });
+    });
+    app.put("/delivery/:id", async (req, res) => {
+      const deliverItems = req.body;
+      const id = req.params.id;
+      // console.log(deliverItems);
+      const filter = { _id: ObjectId(id) };
+      const item = await itemCollection.findOne(filter);
+      const newQuantity = parseInt(item.quantity) - parseInt(deliverItems.quantity);
+      // console.log(item);
+      // console.log(newQuantity);
+      const delivery ={
+        $set: {quantity: newQuantity}
+      }
+  
+      const result = await itemCollection.updateOne(filter, delivery);
+      console.log(result);
+      res.send({
+        success: true,
+        message: `Successfully Update ${deliverItems.name}!`,
       });
     });
   } finally {
